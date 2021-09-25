6ac7db09c9749e5753f08848062095fcda5a1b21
Vanya Dimitrova <vani4ka66@abv.bg>
2021-09-25 09:36:46 +0300

    Commit 7

--- src/Document.js
+++ src/Document.js
@@ -1,61 +1,84 @@
 import './Document.css'
-import React from "react";
-import { Component } from 'react';
+import './App'
+import React, {useState} from "react";
 
-export default class Document extends Component {
-    constructor(props){
-        super(props)
+// export default class Document extends Component {
+//     constructor(props){
+//         super(props)
 
-        this.state = { isRead : false }
-        this._handleScroll = this._handleScroll.bind(this)
+//         this.state = { isRead : false }
+//         this._handleScroll = this._handleScroll.bind(this)
         
-    }
+//     }
 
-    // updateState = () => {
-    //     let content = document.getElementsByClassName('content')[0];
+//     updateState = () => {
+//         let content = document.getElementsByClassName('content')[0];
 
-    //     content.onscroll = function(ev) {
-    //         if ((window.innerHeight + window.scrollY) >= content.offsetHeight) {
+//         content.onscroll = function(ev) {
+//             if ((window.innerHeight + window.scrollY) >= content.offsetHeight) {
 
-    //             this.setState({ isRead : true })
-    //             return this.state
+//                 this.setState({ isRead : true })
+//                 return this.state
                 
-    //         }
-    //         else{
-    //             this.setState({ isRead : false })
-    //             return this.state
+//             }
+//             else{
+//                 this.setState({ isRead : false })
+//                 return this.state
               
-    //         }
-    //     };
-    // }
+//             }
+//         };
+//     }
 
-    // shouldComponentUpdate() {
-    //     return this.state.isRead
-    // }
+//     shouldComponentUpdate() {
+//         return this.state.isRead
+//     }
 
-    _handleScroll = () => {
+//     _handleScroll = () => {
        
-        this.setState({
-            isRead: true
-          })
+//         this.setState({
+//             isRead: true
+//           })
 
-           console.log(this.state.isRead)
-    }
+//            console.log(this.state.isRead)
+//     }
 
-    render(){
-        const {isRead} = this.state
+//     render(){
+//         const {isRead} = this.state
 
-        return <div>
-            <div>{this.isRead}</div>
-                <div className="title">{this.props.title}</div>
-                <div className="content container" onScroll={this._handleScroll}>{this.props.content}</div>
+//         return <div>
+//             <div>{this.isRead}</div>
+//                 <div className="title">{this.props.title}</div>
+//                 <div className="content container" onScroll={this._handleScroll}>{this.props.content}</div>
                         
                 
-                {isRead ?  <button className="allowed" >I agree</button> :
-                    <button  className="notAllowed" >I agree</button> }
+//                 {isRead ?  <button className="allowed" >I agree</button> :
+//                     <button  className="notAllowed" >I agree</button> }
                           
-            </div>
+//             </div>
+//     }
+// }
+
+
+
+export default function Document(props) {
+
+    const [isRead, setisRead] = useState(false)
+
+    const  _handleScroll = () => {
+        setisRead(true)
     }
+
+    return <div>
+                <div>{isRead}</div>
+                    <div className="title">{props.title}</div>
+                    <div className="content container" onScroll={_handleScroll}>{props.content}</div>
+                            
+                    
+                    {isRead ?  <button className="allowed" >I agree</button> :
+                    <button  className="notAllowed" >I agree</button> }
+                              
+            </div>
+    
 }
 
 
