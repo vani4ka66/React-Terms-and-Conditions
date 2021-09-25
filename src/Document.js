b47bf28c35aad50317bf62f2f3b22599bb97e329
Vanya Dimitrova <vani4ka66@abv.bg>
2021-09-25 10:00:32 +0300

    Commit 8

--- src/Document.js
+++ src/Document.js
@@ -1,81 +1,26 @@
-import './Document.css'
-import './App'
 import React, {useState} from "react";
-
-// export default class Document extends Component {
-//     constructor(props){
-//         super(props)
-
-//         this.state = { isRead : false }
-//         this._handleScroll = this._handleScroll.bind(this)
-        
-//     }
-
-//     updateState = () => {
-//         let content = document.getElementsByClassName('content')[0];
-
-//         content.onscroll = function(ev) {
-//             if ((window.innerHeight + window.scrollY) >= content.offsetHeight) {
-
-//                 this.setState({ isRead : true })
-//                 return this.state
-                
-//             }
-//             else{
-//                 this.setState({ isRead : false })
-//                 return this.state
-              
-//             }
-//         };
-//     }
-
-//     shouldComponentUpdate() {
-//         return this.state.isRead
-//     }
-
-//     _handleScroll = () => {
-       
-//         this.setState({
-//             isRead: true
-//           })
-
-//            console.log(this.state.isRead)
-//     }
-
-//     render(){
-//         const {isRead} = this.state
-
-//         return <div>
-//             <div>{this.isRead}</div>
-//                 <div className="title">{this.props.title}</div>
-//                 <div className="content container" onScroll={this._handleScroll}>{this.props.content}</div>
-                        
-                
-//                 {isRead ?  <button className="allowed" >I agree</button> :
-//                     <button  className="notAllowed" >I agree</button> }
-                          
-//             </div>
-//     }
-// }
-
-
+import './Document.css'
 
 export default function Document(props) {
 
     const [isRead, setisRead] = useState(false)
 
-    const  _handleScroll = () => {
-        setisRead(true)
+    const handleScroll = () => {
+
+        let content = document.getElementsByClassName('content')[0];
+       console.log(content.offsetHeight)
+
+            if ((content.scrollTop + content.offsetHeight) >= content.scrollHeight) {
+                setisRead(true)     
+            }
     }
 
     return <div>
-                <div>{isRead}</div>
-                    <div className="title">{props.title}</div>
-                    <div className="content container" onScroll={_handleScroll}>{props.content}</div>
-                            
+                <div className="title">{props.title}</div>
+                <div className="content container" onScroll={handleScroll}>{props.content}</div>   
                     
-                    {isRead ?  <button className="allowed" >I agree</button> :
-                    <button  className="notAllowed" >I agree</button> }
+                {isRead ?  <button className="allowed" >I agree</button> :
+                <button  className="notAllowed" >I agree</button> }
                               
             </div>
     
