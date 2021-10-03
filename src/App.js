dcd8d13347aade3f0366b13b033c62449844d7ea
Vanya Dimitrova <vani4ka66@abv.bg>
2021-10-03 19:42:35 +0300

    Commit 18

--- src/App.js
+++ src/App.js
@@ -21,18 +21,6 @@ function App() {
   return (
     <div className="App">
       <Document title="Terms and Conditions" content={value} />
-
-      <section class="hero">
-        <div class="hero-body">
-          <p class="title">A React Task</p>
-          <p class="subtitle">by Boom.dev</p>
-        </div>
-      </section>
-      <div class="container is-fullhd">
-        <div class="notification">
-          Edit the <code>./src</code> folder to add components.
-        </div>
-      </div>
     </div>
   );
 }
